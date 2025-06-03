4057e841675b91362f60fe90b0570a407ecd4cb0
Elektron <s762672@ya.ru>
2025-06-03 22:21:31 +0300

    Технические шоколадки...

--- test.bat
+++
@@ -1,6 +0,0 @@
-cd Site
-pyinstaller -w -F Site\Broker.py
-pyinstaller -w -F Site\Site.py --add-data templates:templates --add-data static:static
-pyinstaller -w -F Site\main.py -n=WinEd
-cd ..\
-cls
