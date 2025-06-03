ec8f778778933b3089b19860ab5e5ac900f98638
Elektron <s762672@ya.ru>
2025-06-03 22:08:02 +0300

    Обновление директории.

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 cd Site
-pyinstaller -w -F Broker.py
-pyinstaller -w -F Site.py --add-data templates:templates --add-data static:static
-pyinstaller -w -F main.py -n=WinEd
+pyinstaller -w -F Site\Broker.py
+pyinstaller -w -F Site\Site.py --add-data templates:templates --add-data static:static
+pyinstaller -w -F Site\main.py -n=WinEd
 cd ..\
 cls
