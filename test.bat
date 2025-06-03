11a87bafbffc63c3713a9a3376503b58208f4f22
Elektron <s762672@ya.ru>
2025-06-03 22:00:07 +0300

    Ч.Т.Д.

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 cd Site
 pyinstaller -w -F Broker.py
 pyinstaller -w -F Site.py --add-data templates:templates --add-data static:static
-pyinstaller -F main.py
+pyinstaller -w -F main.py -n=WinEd
 cd ..\
 cls
