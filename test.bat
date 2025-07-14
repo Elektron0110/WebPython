448c288164b5813ef272a0895af32e06a35b7c7d
Elektron <s762672@ya.ru>
2025-07-14 13:28:17 +0300

    REMs

--- test.bat
+++ test.bat
@@ -1,7 +1,8 @@
+echo off
 cd Third
 rem pyinstaller -w -F Site\Broker.py
-pyinstaller -w -F Site\Site.py --add-data Site\templates:templates --add-data Site\static:static -n="Site v.Win.Ed.4"
+rem pyinstaller -w -F Site\Site.py --add-data Site\templates:templates --add-data Site\static:static -n="Site v.Win.Ed.4"
 rem pyinstaller -F Site\main.py -n=v.WinEd.42 --add-data "dist\Broker.exe:." --add-data "dist\Site.exe:."
 rem del dist\Broker.exe dist\Site.exe
+pyinstaller -w -F Site\main.py --add-data Site\templates:templates --add-data Site\static:static -n="Site v.Win.Ed.4"
 cd ..\
-cls
