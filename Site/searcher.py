5af7c987b710f16a158d8534f742bb1c8f1542cb
Elektron <s762672@ya.ru>
2025-08-03 10:44:20 +0300

    Города

--- Site/searcher.py
+++ Site/searcher.py
@@ -7,20 +7,20 @@ flight_tracker = fr_api.get_flight_tracker_config()
 flight_tracker.vehicles = "0"
 fr_api.set_flight_tracker_config(flight_tracker)
 Пулково = fr_api.get_airport('LED')
-Иркутск = fr_api.get_airport('IKT')
+Братск = fr_api.get_airport('BTK')
 Толмачёво = fr_api.get_airport('OVB')
 Домодедово = fr_api.get_airport('DME')
 Храброво = fr_api.get_airport('KGD')
-Сочи = fr_api.get_airport('AER')
+Платов = fr_api.get_airport('ROV')
 Елизово = fr_api.get_airport('PKC')
 самолёт: dict[str, Any] = {}
 интерес = {'number': 'Рейс', 'airline_iata': 'Авиакомпания', 'latitude': 'Широта', 'longitude': 'Долгота',
 		   'heading': 'Курс', 'altitude': 'Высота', 'ground_speed': 'Скорость', 'aircraft_code': 'Тип',
 		   'registration': 'Регистрационный номер', 'origin_airport_iata': 'Аэропорт отправления',
 		   'destination_airport_iata': 'Аэропорт прибытия', 'get_distance_from(Пулково)': 'Расстояние до Пулково',
-		   'get_distance_from(Иркутск)': 'Расстояние до Иркутска', 'get_distance_from(Толмачёво)': 'Расстояние до Толмачёво',
+		   'get_distance_from(Братск)': 'Расстояние до Братска', 'get_distance_from(Толмачёво)': 'Расстояние до Толмачёво',
 		   'get_distance_from(Домодедово)': 'Расстояние до Домодедово', 'get_distance_from(Храброво)': 'Расстояние до Храброво',
-		   'get_distance_from(Сочи)': 'Расстояние до Сочи', 'get_distance_from(Елизово)': 'Расстояние до Елизово'}
+		   'get_distance_from(Платов)': 'Расстояние до Платова', 'get_distance_from(Елизово)': 'Расстояние до Елизово'}
 def fly(рейс):
 	компания = str()
 	for знак in рейс:
