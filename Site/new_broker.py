88c20195979d11a3268ed15fbbc8459ebe9778e8
Elektron <s762672@ya.ru>
2025-09-20 21:07:43 +0300

    Sunny update

--- Site/new_broker.py
+++ Site/new_broker.py
@@ -1,11 +1,12 @@
 from flask import send_file, render_template, request
 from flask import Blueprint as Flask
 import matplotlib.pyplot as plt
-import io, os, json
+import io, os, json, math
 from datetime import datetime as dt
 from datetime import timedelta as td
 import plotly.express as px
 import pandas as pd
+from requests import get
 
 app = Flask(__name__, 'new_broker')
 
@@ -72,3 +73,33 @@ def index():
 <a style="color: chocolate; text-decoration: none;" href="{("/Ums?date="+dt_0) if dt_0 else ""}" title="{dt_0 if dt_0 else "Дальше данных нет!"}">-1 день</a>
 <a style="color: chocolate; text-decoration: none;" href="{("/Ums?date="+dt_1) if dt_1 else ""}" title="{dt_1 if dt_1 else "Дальше данных нет!"}">+1 день</a>
 '''
+
+@app.route('/sun')
+def sun():
+	date = request.args.get('date')
+	if not date: date = dt.today().strftime("%Y.%m.%d")
+	dt_0 = (dt.strptime(date, '%Y.%m.%d')-td(1)).strftime('%Y.%m.%d') if dt.strptime(date, '%Y.%m.%d') >= dt(2025, 8, 18) else None
+	dt_1 = (dt.strptime(date, '%Y.%m.%d')+td(1)).strftime('%Y.%m.%d') if dt.strptime(date, '%Y.%m.%d') < dt.today()-td(1) else None
+	# Пример данных
+	open(f'sun/{date}', 'w').write(get(f'https://xras.ru/txt/xray_S0KJ_{date.replace('.','')}.json').text)
+	datae: list[dict[str, str]] = json.loads(open(f'sun/{date}', 'r').read())['data']
+	weather: dict[str, list[dt | float]] = dict(time=[], value=[])
+	for tv in datae:
+		weather['time'].append(dt.strptime(tv['time'], '%Y-%m-%dT%H:%M:%S'))
+		weather['value'].append(math.log(float(tv['long']), 10))
+	df = pd.DataFrame(weather)
+	fig = px.line(df, 'time', 'value', markers=True)
+	fig.update_layout(legend_orientation="h",
+                  legend=dict(x=.5, xanchor="center"),
+                  title='Солнечная активность',
+                  xaxis_title="Время (UTC+3)",
+                  yaxis_title="Вт/м²")
+	# fig.update_yaxes(range=[-0.0000082, 0.0000149], tickvals=[-0.0000082, -0.0000036, 0.000001, 0.0000056, 0.0000102, 0.0000149], ticktext=['A', 'B', 'C', 'M', 'X', 'X10'])
+	# fig.update_yaxes(range=[-0.0000082, 0.0000149], tickvals=[10**-8,  10**-7, 10**-6, 10**-5, 10**-4, 10**-3], ticktext=['A', 'B', 'C', 'M', 'X', 'X10'])
+	# fig.update_yaxes(range=[-0.0000082, 0.0000149])
+	# fig.update_yaxes(tickvals=[10**-6, 10**-5, 10**-4], ticktext=['C', 'M', 'X'])
+	# fig.update_yaxes(tickvals=[10**(-a) for a in range(2, 10)], ticktext=[f'10**{(-a)}' for a in range(2, 10)])
+	fig.update_yaxes(tickvals=[-8, -7, -6, -5, -4], ticktext=['A', 'B', 'C', 'M', 'X'])
+	graph_html = fig.to_html(full_html=False)
+
+	return render_template(f'sun.html', graph=graph_html, dt_0=dt_0, dt_1=dt_1)
