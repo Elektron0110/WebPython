318876d1fc0c40254b7096d6b6f33c262aacf03a
Elektron <s762672@ya.ru>
2026-06-24 09:12:49 +0300

    LogCleaner

---
+++ Site/log_cleaner.py
@@ -0,0 +1,14 @@
+import os
+
+for file in [file for file in os.listdir() if file.endswith('.log')]:
+    last_line = ''
+
+    f = open(file, 'r', encoding='utf-8').readlines()
+    s = open(file+'.new', 'w', encoding='utf-8')
+
+    for line in f:
+        if line.split('  ')[2:] == last_line.split('  ')[2:]:
+            print(f.index(line), line)
+            continue
+        s.write(line)
+        last_line = line
