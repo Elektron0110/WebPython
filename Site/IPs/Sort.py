cc6f545b27d764465289e9bd3d80052b39a89c47
Elektron <s762672@ya.ru>
2026-08-30 20:54:00 +0300

    Улучшения

--- Site/IPs/Sort.py
+++ Site/IPs/Sort.py
@@ -4,34 +4,36 @@ from time import sleep
 
 file = open('Site/Alexis.log', 'r', encoding='utf-8').readlines()
 
-try:
-    # Для работы этого блока его необходимо перенести в оновную папку (Site)
-    from IPs import IP_Seeker
+print([i+1 for i in range(len(file)) if len(file[i]) < 23 or file[i][22]!=file[i][21]])
 
-    print([i+1 for i in range(len(file)) if len(file[i]) < 23 and file[i][22]!=file[i][21]])
+try:
+    # Для работы этого блока, файл необходимо перенести в оновную папку (Site).
+    from IPs import IP_Seeker
 
     a = list({line.split('  ')[1] for line in file
         if (line[23].isdigit() and line.split('  ')[1] not in [f2[:-3] for f2 in os.listdir('Site/IPs')])})
     a.sort()
     print(len(a))
     for ip in a:
+        print(ip)
         IP_Seeker(ip, 'Site').Seek()
         sleep(10)
 except:
 
 
-    def mprint(object: dict[str, list]):
+    def mprint(object: dict[str, list], max=-1):
+        import builtins
         string = ''
         keys = [key for key in object]
         keys.sort()
-        m_len = max([len(key) for key in keys])
+        m_len = builtins.max([len(key) for key in keys])
         for key in keys:
             half = (m_len-len(key))//2
             half+0 if (m_len-len(key))%2 else 0
             if object[key] == []: object[key] = ['']
             object[key].sort()
-            string += f'{half*' '}{key}{(half+(1 if (m_len-len(key))%2 else 0))*' '} | {object[key][0]}'+'\n'
-            if len(object[key]) > 1: string += '\n'.join([f'{m_len*' '} | {value}' for value in object[key][1:]])+'\n'
+            string += f'{half*' '}{key}{(half+(1 if (m_len-len(key))%2 else 0))*' '} | {object[key][0]}'[:max]+'\n'
+            if len(object[key]) > 1: string += '\n'.join([f'{m_len*' '} | {value}'[:max] for value in object[key][1:]])+'\n'
         print(string)
         return string
 
@@ -40,6 +42,7 @@ except:
     for f in [f for f in os.listdir('Site/IPs')[1:-2]
         if len(open(f'Site/IPs/{f}', 'r', encoding='utf-8').readlines()) < 7]: os.remove(f'Site/IPs/{f}')
 
+    print(len([f for f in os.listdir('Site/IPs')[1:-2] if ('RU' not in open(f'Site/IPs/{f}', 'r', encoding='utf-8').read()) and (f[:-3] not in [f2[:-4] for f2 in os.listdir('Site/IPs/I')])]))
     print([f for f in os.listdir('Site/IPs')[1:-2] if ('RU' not in open(f'Site/IPs/{f}', 'r', encoding='utf-8').read()) and (f[:-3] not in [f2[:-4] for f2 in os.listdir('Site/IPs/I')])])
     for f in [f for f in os.listdir('Site/IPs')[1:-2]
             if ('RU' not in open(f'Site/IPs/{f}', 'r', encoding='utf-8').read()) and
@@ -53,4 +56,4 @@ except:
         if i not in counter: counter[i] = []
         counter[i] += [f'{l.split('  ')[0][1:-1]} | {l.split('  ')[2][l.split('  ')[2].find(' ')+1:-1]}'
                     for l in file if f[:-4] in l]
-    open('CS.txt', 'w', encoding='utf-8').write(mprint({k: v for k, v in counter.items() if k}))
+    open('CS.txt', 'w', encoding='utf-8').write(mprint({k: v for k, v in counter.items() if k}, max=100))
