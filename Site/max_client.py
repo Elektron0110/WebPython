84d50ac114a0e7b3064496d32f995500394d7855
Elektron <s762672@ya.ru>
2026-09-02 07:54:18 +0300

    /max

--- Site/max_client.py
+++ Site/max_client.py
@@ -8,20 +8,22 @@ app = Flask(__name__, 'new_broker')
 
 @app.route('/max')
 def max():
-    max_data: dict[str, list[dict[str, str]]] = json.load(open('max_messages.json'))
+    max_data: dict[int, list[dict[str, str]]] = json.load(open('max_messages.json'))
     default_chat = max_data[default]
     return jsonify(default_chat)
 
 
 @app.route('/max/all')
 def maxall():
-    chats: list[dict[str, str]] = ... # json.load(open('max_chats.json'))
-    fstring = f'<a href="/max/{chats[0]["id"]}">{chats[0]["name"]}</a>'
+    chats: list[dict[str, str]] = json.load(open('max_chats.json'))
+    fstring = f'<a href="/max/{chats[0]["id"]}">{chats[0]["type"]} | {chats[0]["name"]}</a>'
     return fstring+'</a>\n<br>\n'.join([f'<a href="/max/{chat["id"]}">{chat["name"]}' for chat in chats[1:]])
 
 
 @app.route('/max/<x>')
 def maxx(x):
-    max_data: dict[str, list[dict[str, str]]] = json.load(open('max_messages.json'))
+    open('HELPFILE','w').write(x)
+    while open('HELPFILE').read() != 'DONE': pass
+    max_data: dict[int, list[dict[str, str]]] = json.load(open('max_messages.json'))
     chat = max_data[x]
     return jsonify(chat)
