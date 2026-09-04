67f0fbebc9e371886584696bb6dc2edf81493ef8
Elektron <s762672@ya.ru>
2026-09-04 19:32:08 +0300

    file

--- Site/Max.py
+++ Site/Max.py
@@ -265,6 +265,7 @@ async def show_chat_history(client: Client, id: str | int):
 
             text = getattr(msg, 'text', '') or ''
             date = msg.time
+            file = None
             for attachment in msg.attaches:
                 info = [k for k in attachment.__dict__]
                 try:
@@ -273,11 +274,12 @@ async def show_chat_history(client: Client, id: str | int):
                 except:
                     print(attachment.__dict__)
                     continue
-                if f'{msg.id}|{media_id}' not in [f[:-5] for f in os.listdir('static/max')]:
-                    open(f'static/max/{msg.id}_{media_id}.file', 'wb').write(get(base_url).content)
+                file = f'{msg.id}|{media_id}'
+                if file not in [f[:-5] for f in os.listdir('static/max')]:
+                    open(f'static/max/{file}.file', 'wb').write(get(base_url).content)
             time_str = (datetime(1970, 1, 1)+timedelta(days=date/1000/3600/24)+timedelta(hours=3)).strftime('%Y.%m.%d %H:%M:%S')
 
-            messes.append({"time": time_str, "sender": sender_name, "text": text})
+            messes.append({"time": time_str, "sender": sender_name, "text": text, "file": file})
         return messes
 
 # ========== ОБРАБОТЧИК ВХОДЯЩИХ СООБЩЕНИЙ ==========
